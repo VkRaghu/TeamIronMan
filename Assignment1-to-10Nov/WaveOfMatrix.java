e38c5a1ae99c037c8df0ccc69f81f61bf052fe51
VkRaghu <90498029+VkRaghu@users.noreply.github.com>
2021-11-14 12:44:19 +0530

    Add files via upload

---
+++ Assignment1-to-10Nov/WaveOfMatrix.java
@@ -0,0 +1,38 @@
+package twoDarray;
+
+public class WaveOfMatrix {
+	
+	public static void main(String[] args) {
+		int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		
+		int top = 0, botm = arr.length-1;
+		int upper = 0;
+		int dir = 0;
+		
+		while(upper<arr.length) {
+			if(dir==0) {
+				for(int i=top; i<=botm; i++) {
+					System.out.print(arr[i][upper]+" ");
+				}
+				upper++;
+			}else if(dir==1) {
+				for(int i=botm; i>=top; i--) {
+					System.out.print(arr[i][upper]+" ");
+				}
+				upper++;
+			}else if(dir==2) {
+				for(int i=top; i<=botm; i++) {
+					System.out.print(arr[i][upper]+" ");
+				}
+				upper++;
+			}else if(dir==3) {
+				for(int i=botm; i>=top; i--) {
+					System.out.print(arr[i][upper]+" ");
+				}
+				upper++;
+			}
+			dir = (dir+1)%4;
+		}
+	}
+
+}
