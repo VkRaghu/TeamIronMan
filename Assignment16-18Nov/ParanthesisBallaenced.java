d8ca960fa2b7e09e348d51ca0bab3e3d3c4ca611
VkRaghu <90498029+VkRaghu@users.noreply.github.com>
2021-11-20 10:01:16 +0530

    Add files via upload

---
+++ Assignment16-18Nov/ParanthesisBallaenced.java
@@ -0,0 +1,30 @@
+package stack;
+
+import java.util.Stack;
+
+public class ParanthesisBallaenced {
+	
+	public static void main(String[] args) {
+		
+		String str = "(())()()()";
+		
+		Stack<Character> st = new Stack<>();
+		for(int i=0; i<str.length(); i++) {
+			
+			if(str.charAt(i)=='(') {
+				st.push(str.charAt(i));
+			}
+			else if(!st.empty() && (str.charAt(i)==')' && st.peek()=='(')) {
+				st.pop();
+			}
+			else {
+				st.push(str.charAt(i));
+			}
+		}
+		if(st.empty()) {
+			System.out.println("Balanced");
+		}
+		else System.out.println("Not ballanced");
+	}
+
+}
